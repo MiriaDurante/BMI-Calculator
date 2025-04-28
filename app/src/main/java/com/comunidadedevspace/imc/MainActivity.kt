a9cfd346f4f6edf58543bf1f87fd33b11f7f352c
MiriaDurante <miriaduranteit@gmail.com>
2025-04-28 10:44:50 +0200

    Classificação segunda página

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
@@ -42,6 +44,12 @@ class MainActivity : AppCompatActivity() {
 
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
+
+                //Navegar para a prox tela (Intent - classe) e criar layout
+
+                val intent = Intent(this,ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC ,resultado)
+                startActivity(intent)
 
                 println("ação do botão" + resultado)
             }
